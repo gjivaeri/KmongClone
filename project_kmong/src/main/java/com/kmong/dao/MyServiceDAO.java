55b5cb06e0e189b1c1e35bb02ebb739d4da09aa4
user <user@DESKTOP-COJ9858>
2022-04-30 22:20:36 +0900

    cyy commit

--- project_kmong/src/main/java/com/kmong/dao/MyServiceDAO.java
+++ project_kmong/src/main/java/com/kmong/dao/MyServiceDAO.java
@@ -107,21 +107,21 @@ public class MyServiceDAO {
 	    	System.out.println("DB연결 성공");
 	    	
 	    	insertMyService
-	    		.append("insert into post(post_id, title, summary, price, term, description, post_img, category_id)	")
-	    		.append("values(post_seq.nextval,?,?,?,?,?,?,?)	");
+	    		.append("insert into post(post_id, member_id, title, summary, price, term, description, post_img, category_id)	")
+	    		.append("values(post_seq.nextval,?,?,?,?,?,?,?,?)	");
 	    		//.append("where member_id=?	");
 	    	
 	    	pstmt=con.prepareStatement(insertMyService.toString());
 	    	
-	    	pstmt.setString(1, pVO.getTitle());
-	    	//pstmt.setInt(2, pVO.getMemberId());
-	    	pstmt.setString(2, pVO.getSummary());
-	    	pstmt.setInt(3, pVO.getPrice());
-	    	pstmt.setInt(4, pVO.getTerm());
+	    	pstmt.setInt(1, pVO.getMemberId());
+	    	pstmt.setString(2, pVO.getTitle());
+	    	pstmt.setString(3, pVO.getSummary());
+	    	pstmt.setInt(4, pVO.getPrice());
+	    	pstmt.setInt(5, pVO.getTerm());
 	    	//pstmt.setString(5, pVO.getPostDate());
-	    	pstmt.setString(5, pVO.getDescription());
-	    	pstmt.setString(6, pVO.getPostImg());
-	    	pstmt.setInt(7, pVO.getCategoryId());
+	    	pstmt.setString(6, pVO.getDescription());
+	    	pstmt.setString(7, pVO.getPostImg());
+	    	pstmt.setInt(8, pVO.getCategoryId());
 	    	//pstmt.setInt(9, pVO.getMemberId());
 	    	
 	    	pstmt.executeUpdate();
